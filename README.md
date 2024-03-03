0b28fdbd8af702293128b258a318ae70101871dc
Carson Hartley <chartley1988@gmail.com>
2024-03-03 10:05:48 -0700

    add some text about getting out of jail, get some cash, some colour functionality

--- README.md
+++ README.md
@@ -2,16 +2,24 @@
 
 ## The pitch
 
-The goal is to make a pure and plain text based game that runs in the terminal. This is a collaborative adventure, involving two or more creators. The game can be created in any language, so long as it outputs to terminal.
+The goal is to make a pure and plain text based game that runs in the terminal.
+This is a collaborative adventure, involving two or more creators. The game can
+be created in any language, so long as it outputs to terminal.
 
 ## Why make this?
 
-It encourages best practices, and keeps it fun Rules are aimed to keep it small, so it's pretty low stress. No need to manage files, dependencies or configs It's a creative outlet Variations could be made where some mutually agreed assets such as packages, databases, and config files are allowed. These would be starters.
+It encourages best practices, and keeps it fun Rules are aimed to keep it
+small, so it's pretty low stress. No need to manage files, dependencies or
+configs It's a creative outlet Variations could be made where some mutually
+agreed assets such as packages, databases, and config files are allowed. These
+would be starters.
 
 ## The Rules
 
-The participants must alternate commits, no two in a row by one person, even for bug fixes. This puts a bit of pressure to do high quality single commits. No small change here.
-One file - A single text file. This is good practice for code organization, navigating code, and reading others code. Git files are fine.
-No images or external files of any kind.
-No external code packages either, if we want it we build it.
-You may only spend 30 minutes on the computer per commit. Phones count as a computer.
+The participants must alternate commits, no two in a row by one person, even
+for bug fixes. This puts a bit of pressure to do high quality single commits.
+No small change here. One file - A single text file. This is good practice for
+code organization, navigating code, and reading others code. Git files are
+fine. No images or external files of any kind. No external code packages
+either, if we want it we build it. You may only spend 30 minutes on the
+computer per commit. Phones count as a computer.
--- app.py
+++ app.py
@@ -1,47 +1,101 @@
-# I know we said no packages, but I added a virtual environment anyways. 
+# I know we said no packages, but I added a virtual environment anyways.
 # To create a venv
 # python -m venv (source name... i always just use venv)
 # To activate the virtual env run:
 # source/{the name you picked (venv)}/bin/activate
 # You'll see a (venv) in front of your terminal, youre in!
-# pip install -r requirements.txt will install all requirements (do this in your venv or they will go global) 
-# pip freeze > requirements.txt  (will write all imports you've added to the requirements)
+# pip install -r requirements.txt will install all requirements (do this in
+# your venv or they will go global) pip freeze > requirements.txt  (will write
+# all imports you've added to the requirements)
 # Will save the current config to the requirements
-#####################################################################################
+###############################################################################
 
 
 def main():
-    # Use variabless here or go right into objects (dicts in python)? Or a Class? You Pick, delete the rest ;)
+    # Use variables here or go right into objects (dicts in python)? Or a
+    # Class? You Pick, delete the rest ;)
 
-    # Variable Option
-    name = getName()
-    print(name)
+    class colour:
+        HEADER = "\033[95m"
+        BLUE = "\033[94m"
+        CYAN = "\033[96m"
+        GREEN = "\033[92m"
+        YELLOW = "\033[93m"
+        RED = "\033[91m"
+        ENDC = "\033[0m"
+        BOLD = "\033[1m"
+        UNDERLINE = "\033[4m"
 
-    # Dict option
-    player_dict = {
-        'name': name
-    }
-    print(player_dict["name"]) # Note python doesn't do dot notation... PlayerDict.name is a no bueno
-    # print(PlayerDict.name) #This will be a syntax error
+        def demoColours():
+            print(
+                f"""
+colour Demo
+{colour.HEADER}Header{colour.ENDC}
+{colour.BLUE}Blue{colour.ENDC}
+{colour.GREEN}Green{colour.ENDC}
+{colour.CYAN}Cyan{colour.ENDC}
+{colour.YELLOW}Yellow{colour.ENDC}
+{colour.RED}Red{colour.ENDC}
+{colour.BOLD}Bold{colour.ENDC}
+{colour.UNDERLINE}Underlined{colour.ENDC}
+{colour.ENDC}End colour formatting
+"""
+            )
 
-    # Class option (I haven't got much into python classes myself)
     class PlayerClass:
-        def __init__(self, name):
-            self.name = name
+        def __init__(self):
+            self.name = ""
+            self.money = 0
+
         def changeName(self, newName):
             self.name = newName
-    player_class = PlayerClass(name)
-    print(player_class.name)
-    player_class.changeName('Chizzartley')
-    print(player_class.name)
+
+        def addMoney(self, amount):
+            if isinstance(amount, int):
+                self.money += amount
+                print(
+                    f"{colour.YELLOW}{amount}${colour.ENDC} added to your wallet! \nCurrent Balance: {colour.YELLOW}{self.money}${colour.ENDC}"
+                )
+            else:
+                print("This ain't money...")
+
+    # Creates player instance
+    player = PlayerClass()
+    player.changeName(enterName())
+
+    # Player is getting released from jail, and the guard releasing you is
+    # trying to be encouraging, but not doing a great job.
+    print(
+        """
+JAILER
+The good news is, you're finally free. You've been locked up for so
+many years, must feel good! The bad news is, this probably isn't the
+world you remember. You don't have a penny to your name, and your
+possessions are worth as much as your wallet.
+    """
+    )
+
+    print(
+        """
+JAILER 
+Your family felt bad for you though, and they've sent you some money. It should
+be enough to buy yourself lunch.
+          """
+    )
+
+    player.addMoney(40)
+
+    colour.demoColours()
 
     return None
 
+
 # A function which will provide the name for the main func.
-def getName():
-    name = str(input('\nHello Adventurer! What shall I call you?\n'))   
-    print(f'\nWell it\'s a pleasure to meet you, {name}!\n')
+def enterName():
+    name = str(input("\nHello Adventurer! What shall I call you?\n"))
+    print(f"\nWell it's a pleasure to meet you, {name}!")
     return name
+
 
 # End the program with the main function call
 main()
