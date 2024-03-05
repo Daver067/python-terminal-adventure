fdc433c3e09c88e28f4dced94d1392fce6a777da
Carson Hartley <chartley1988@gmail.com>
2024-03-05 16:35:43 -0700

    add a multiple-choice selection query for branching dialogs, also added an error display helper

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 import sys
+
 # I know we said no packages, but I added a virtual environment anyways.
 # To create a venv
 # python -m venv (source name... i always just use venv)
@@ -14,35 +15,6 @@ import sys
 
 
 def main():
-    class colour: 
-        HEADER = "\033[95m"
-        BLUE = "\033[94m"
-        CYAN = "\033[96m"
-        GREEN = "\033[92m"
-        YELLOW = "\033[93m"
-        RED = "\033[91m"
-        ENDC = "\033[0m"
-        BOLD = "\033[1m"
-        UNDERLINE = "\033[4m"
-
-        def money(amount):
-            return (f"{colour.YELLOW}${amount}{colour.ENDC}")
-
-        def demoColours():
-            print(
-                f"""
-colour Demo
-{colour.HEADER}Header
-{colour.BLUE}Blue
-{colour.GREEN}Green
-{colour.CYAN}Cyan
-{colour.YELLOW}Yellow
-{colour.RED}Red{colour.ENDC}
-{colour.BOLD}Bold{colour.ENDC}
-{colour.UNDERLINE}Underlined
-{colour.ENDC}End colour formatting
-"""
-            )
 
     class PlayerClass:
         def __init__(self):
@@ -56,7 +28,10 @@ colour Demo
             if isinstance(amount, int):
                 self.money += amount
                 delay_print(
-                    f"{colour.money(40)} added to your wallet! \nCurrent Balance: {colour.money(self.money)}"
+                    f"""
+        {colour.money(40)} added to your wallet!
+        Current Balance: {colour.money(self.money)}
+"""
                 )
             else:
                 delay_print("This ain't money...")
@@ -79,26 +54,112 @@ colour Demo
         I have a wallet?!
 
 {colour.GREEN}JAILER{colour.ENDC}
-        Your family felt bad for you though, and they've sent you some money. It should
-        be enough to buy yourself lunch.
-          """
+        Your family felt bad for you though, and they've sent you some money.
+        It should be enough to buy yourself lunch.
+"""
     )
     player.addMoney(40)
+
+    delay_print(
+        f"""
+{colour.CYAN}NARRATOR{colour.ENDC}
+        And so, {player.name} leaves the prison that has been home for so long. 
+        Where should {player.name} go now?
+        """
+    )
+
+    showChoices(
+        [
+            "Lunch sounds like a good idea",
+            "Might be a good idea to see the family!",
+            "I'm feeling lucky, I'm gonna hit the casino",
+        ]
+    )
+
     return None
 
 
 # A function which will provide the name for the main func.
 def enterName():
-    name = str(input("\nHello Adventurer! What shall I call you?\n")) #TODO can we make this delay_print? We will probably have to make a delay_input... I couldn't make it work
+    name = str(
+        input("\nHello Adventurer! What shall I call you?\n")
+    )  # TODO can we make this delay_print? We will probably have to make a delay_input... I couldn't make it work
     delay_print(f"\nWell it's a pleasure to meet you, {name}!")
     return name
 
+
 # Prints things slowly, the LOWER the speed the faster the typing
-def delay_print(str, speed = 4):
+def delay_print(str, speed=1):
     for char in str:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(speed/100)
+        time.sleep(speed / 100)
+
+
+# Shows a list of options, and gives them a number for each. Player must type in a number to make a selection.
+def showChoices(options):
+    # Check if inputs are the right type
+    if not isinstance(options, list):
+        showError("Developer! Make sure your options are a list of strings!")
+        return
+
+    print("\n")  # Controls format of options so they reset their indent
+    for index, item in enumerate(options):
+        # Check to make sure option is a string
+        if not (isinstance(item, str)):
+            showError("Developer! the option must be a string.")
+            return
+        print(f"{colour.BLUE}[{index}]{colour.ENDC} - {item}")
+    answer = None
+    while (
+        not isinstance(answer, int) or not (answer >= 0) or not (answer < len(options))
+    ):
+        answer = input()
+        if not (answer.isdigit()):
+            showError("Not a valid answer, please enter a digit for your selection")
+        if answer.isdigit():
+            answer = int(answer)
+            if not (answer >= 0):
+                showError("Please enter a number greater than 0!")
+            if not (answer < len(options)):
+                showError("Please select a number within the range of options!")
+    return
+
+
+# Helper function for displaying errors
+def showError(error):
+    print(f"\n{colour.RED}{error}{colour.ENDC}")
+
+
+class colour:
+    HEADER = "\033[95m"
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    def money(amount):
+        return f"{colour.YELLOW}${amount}{colour.ENDC}"
+
+    def demoColours(self):
+        print(
+            f"""
+colour Demo
+{colour.HEADER}Header
+{colour.BLUE}Blue
+{colour.GREEN}Green
+{colour.CYAN}Cyan
+{colour.YELLOW}Yellow
+{colour.RED}Red{colour.ENDC}
+{colour.BOLD}Bold{colour.ENDC}
+{colour.UNDERLINE}Underlined
+{colour.ENDC}End colour formatting
+"""
+        )
 
 
 # End the program with the main function call
