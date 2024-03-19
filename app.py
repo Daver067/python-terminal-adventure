61575d021964be8b92d2b01370e420cc15b7c76c
Dave Diemert <davediemert_88@hotmail.com>
2024-03-19 12:54:21 +0700

    re-structured story routes into an inner function call.
    now when you make a story route, add the function to the paths function, and add the new function to
    paths return value in the format:
    'my_new_func': my_new_func,
    so the key is named the same as the func.

--- app.py
+++ app.py
@@ -15,94 +15,120 @@ import time
 ###############################################################################
 
 
-def main():
-
-    # Creates player instance
+def main(): 
+    # Creates player instance (must happen before paths are defined)
     player = Player()
-    player.set_name(enter_name())
-    # Player is getting released from jail, and the guard releasing you is
-    # trying to be encouraging, but not doing a great job.
-    delay_print(
-        f"""
+    player.set_name(str(delay_input("\nHello Adventurer! What shall I call you?\n")))
 
-        {Colour.GREEN}JAILER{Colour.ENDC}
-        The good news is, you're finally free. You've been locked up for so
-        many years, must feel good! The bad news is, this probably isn't the
-        world you remember. You don't have a penny to your name, and your
-        possessions are worth as much as your wallet.
+    ########################################################################
+    ###############################  PATHS  ################################
+    ########################################################################
+    def game_paths():
+        def start_game():
+            # Player is getting released from jail, and the guard releasing you is
+            # trying to be encouraging, but not doing a great job.
+            delay_print(
+                f"""
 
-        {Colour.BLUE}{player.name}{Colour.ENDC}
-        I have a wallet?!
+                {Colour.GREEN}JAILER{Colour.ENDC}
+                The good news is, you're finally free. You've been locked up for so
+                many years, must feel good! The bad news is, this probably isn't the
+                world you remember. You don't have a penny to your name, and your
+                possessions are worth as much as your wallet.
 
-        {Colour.GREEN}JAILER{Colour.ENDC}
-        Your family felt bad for you though, and they've sent you some money.
-        It should be enough to buy yourself lunch.
-        """
-    )
-    player.add_money(50)
+                {Colour.BLUE}{player.name}{Colour.ENDC}
+                I have a wallet?!
 
-    delay_print(
-        f"""
-        {Colour.CYAN}NARRATOR{Colour.ENDC}
-        And so, {player.name} leaves the prison that has been home for so long.
-        Where should {player.name} go now?
-        """
-    )
+                {Colour.GREEN}JAILER{Colour.ENDC}
+                Your family felt bad for you though, and they've sent you some money.
+                It should be enough to buy yourself lunch.
+                """
+            )
+            player.add_money(50)
 
-    def go_for_lunch():
-        delay_print(
-            f"""
-        You see the allure of the Golden Arches ahead and know you
-        can\'t resist. You are reaching for the door handle when it
-        comes flying into your face. Blood streams down from your nose. 
-        You take {player.print_damage(1, 'point of damage')}. The man walks right by
-        you and gets into a black BMW.
+            delay_print(
+                f"""
+                {Colour.CYAN}NARRATOR{Colour.ENDC}
+                And so, {player.name} leaves the prison that has been home for so long.
+                Where should {player.name} go now?
+                """
+            )
+            match show_choices(
+                [
+                    "Lunch sounds like a good idea",
+                    "Might be a good idea to see the family!",
+                    "I'm feeling lucky, I'm gonna hit the casino",
+                ]
+            ):
+                case "0":
+                    return paths['go_for_lunch']()
+                case "1":  # TODO: make game routes
+                    print("see_the_fam")
+                case "2":     # TODO: make game routes
+                    print("go_to_the_rippers")
 
-        {player.name} is pissed. As he cleans the blood the best he can, a
-        homeless man sitting against the wall near by coughs, and then
-        speaks.
 
-        {Colour.CYAN}HOMELESS MAN{Colour.ENDC}
-        Nasty, dude! And I thought I had bad luck. But you know, luck is a
-        powerful thing, and in times like this it helps to think of karma.
-        Let me give you a gift.
+        def go_for_lunch():
+            delay_print(
+                f"""
+            You see the allure of the Golden Arches ahead and know you
+            can\'t resist. You are reaching for the door handle when it
+            comes flying into your face. Blood streams down from your nose. 
+            You take {player.print_damage(1, 'point of damage')}. The man walks right by
+            you and gets into a black BMW.
 
-        The homeless man gives you two dice, one has four sides, the other
-        six.
-            """
-        )
-        dice_4 = Dice(4)
-        dice_6 = Dice(6)
-        tank = OxygenTank()
-        player.add_item(dice_4)
-        player.add_item(dice_6)
-        player.add_item(tank, 42)
-        player.print_items()
+            {player.name} is pissed. As he cleans the blood the best he can, a
+            homeless man sitting against the wall near by coughs, and then
+            speaks.
 
-        dice = player.query_item(dice_6)
-        if dice:
-            result = dice.roll_dice()
+            {Colour.CYAN}HOMELESS MAN{Colour.ENDC}
+            Nasty, dude! And I thought I had bad luck. But you know, luck is a
+            powerful thing, and in times like this it helps to think of karma.
+            Let me give you a gift.
 
-        show_choices(["Jot down his license plate", "Keep your head down and head in"])
-        return None
+            The homeless man gives you two dice, one has four sides, the other
+            six.
+                """
+            )
+            player.add_item(Dice(4))
+            player.add_item(Dice(6))
+            player.add_item(OxygenTank(), 42)
+            player.print_items()
+            dice = player.query_item(Dice(6))
+            if dice:
+                result = dice.roll_dice()
+            match show_choices([
+                "Jot down his license plate", 
+                "Keep your head down and head in", # TODO: make game routes
+                ]):
+                case "0":
+                    delay_print('You write down his license plate in your notebook') # TODO: need a notebook in inventory
+                    return paths['go_in_restaurant']()
+                case "1":
+                    delay_print('So, you\'re a bit of a coward huh? Go on in then, wuss.\n')
+                    return paths['go_in_restaurant']()
 
-    match show_choices(
-        [
-            "Lunch sounds like a good idea",
-            "Might be a good idea to see the family!",
-            "I'm feeling lucky, I'm gonna hit the casino",
-        ]
-    ):
-        case "0":
-            go_for_lunch()
-        case "1":
-            print("see the fam")
-        case "2":
-            print("go for lunch")
 
-    # TODO: Use these 'TODO:' notes to mark where we leave off for easy access
+        def go_in_restaurant():
+            delay_print('you go in the food house')
+            return None # TODO: make game routes
+        
 
-    return None
+        return {
+            'start_game': start_game,
+            'go_for_lunch': go_for_lunch,
+            'go_in_restaurant': go_in_restaurant,
+            }   
+    ############################################################################
+    #############################  END PATHS  ##################################
+    ############################################################################
+
+    # Initiate the paths to be used below... mostly i think they will just link to eachother above though. But now no hoisting issue
+    paths = game_paths()
+    paths['start_game']()
+
+    return None #! This is the end of main()
+
 
 
 class Item:
@@ -221,15 +247,6 @@ Colour Demo
         )
 
 
-# A function which will provide the name for the main func.
-def enter_name():
-    name = str(delay_input("\nHello Adventurer! What shall I call you?\n"))
-    # TODO: can we make this delay_print? We will probably have to make a
-    # delay_input... I couldn't make it work delay_print(f"\nWell it's a
-    # pleasure to meet you, {name}!")
-    return name
-
-
 # Prints things slowly, the LOWER the speed the faster the typing
 def delay_input(str, delay=1):
     for char in str:
@@ -239,6 +256,7 @@ def delay_input(str, delay=1):
     return input("")
 
 
+# prints things with a dalay in micro seconds
 def delay_print(str, delay=1):
     for char in str:
         sys.stdout.write(char)
@@ -295,6 +313,19 @@ def print_error(error):
 #
 # I also did some other general refactoring so that my linter stopped yelling at
 # me haha. You'll figure it out.
+
+#! Dave:
+#! my issue with declaring a func and then using it makes the script basically read backwards,
+#! when a game like this will need to run unilaterally. Think goosebumps - turn to page 72. now turn to page 13. now 73.
+#! think how it is now, first choice results in choices ABC, which all have to be declared above the initial choice
+#! Then each ABC will have a1 b1 c1 a2 b2 c2...etc which will in turn have to be above... but if c3 links back to b1's
+#! function/storyline you will get an error because c3 function declaration is before b1. 
+#! We need either:
+    #! 1: scope all story routes in global
+    #! 2: scope all story routes in an inner function
+    #! 3: break a rule and use a separate helper module
+#! just thinking about this has already put me into my 30 minute limit haha, I'm going to pull off #2 before i commit
+
 
 
 # End the program with the main function call
